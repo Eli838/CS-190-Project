74bbff002b159dd17af05c2c9842b99d828664de
Sherry Tseng <tsengs3@uci.edu>
2024-05-15 21:28:24 -0700

    a version of rowwise

--- quantization.py
+++ quantization.py
@@ -2,29 +2,35 @@ import torch
 import torch.nn as nn
 import math
 
-import bitsandbytes as bnb
-#from bnb.nn import Linear8bitLt
+
+def quantize_rowwise(x: torch.Tensor):
+    abso = torch.abs(x)
+    output_maxs  = torch.max(abso,1)[0].unsqueeze(-1)
+    output = x[0]  / output_maxs[0,None]
+
+    return output, output_maxs
+
+def dequantize_rowwise(x: torch.Tensor, state_x: torch.Tensor):
+    output = x * state_x
+    return output
+
 
 fp16_model = nn.Sequential(
     nn.Linear(64, 64),
     nn.Linear(64, 64)
 )
 
-int8_model = nn.Sequential(
-    bnb.nn.Linear8bitLt(64, 64, has_fp16_weights=False),
-    bnb.nn.Linear8bitLt(64, 64, has_fp16_weights=False)
-)
 
-print("unquantized")
+print("   unquantized")
 print(fp16_model.state_dict()['0.weight'][0])
 #print(bnb.triton.quantize_rowwise.quantize_rowwise)
-testing, testmax = bnb.triton.quantize_rowwise.quantize_rowwise(fp16_model.state_dict()['0.weight'][0])
+testing, testmax = quantize_rowwise(fp16_model.state_dict()['0.weight'])
 
 print("quantized")
 print(testing[0])
 
 print("dequantized")
-print(bnb.triton.dequantize_rowwise.dequantize_rowwise(testing,testmax)[0])
+print(dequantize_rowwise(testing,testmax)[0])
 #int8_model.load_state_dict(fp16_model.state_dict())
 #print(torch.cuda.is_available())
 #int8_model = int8_model.to(0) # Quantization happens here
