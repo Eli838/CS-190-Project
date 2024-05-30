8144269c3a41a447707c3eaa82d2eed5e451620b
Sherrytng <103454100+Sherrytng@users.noreply.github.com>
2024-05-29 20:56:21 -0700

    Update quantization.py

--- quantization.py
+++ quantization.py
@@ -1,5 +1,4 @@
 
-
 import torch
 import torch.nn as nn
 import math
@@ -33,36 +32,37 @@ def calc_exp(exp, new_exp_len):
       return limit
   if val < -limit + 1:
       return -limit + 1
-
+      pass
   return val
 
+
 def round_fp8(x, exp = 4):
-'''
-Quantizes input tensor to FP8 data format
-inputs  x:      original tensor
-        exp:    number of bits used for exponent field
-                e.g. E5M2 has 5 exp bits, E4M3 has 4 exp bits
-output  x_32:   quantized tensor
-'''
+  '''
+  Quantizes input tensor to FP8 data format
+  inputs  x:      original tensor
+          exp:    number of bits used for exponent field
+                  e.g. E5M2 has 5 exp bits, E4M3 has 4 exp bits
+  output  x_32:   quantized tensor
+  '''
 
-x_fp8 = copy.deepcopy(x)
+  x_fp8 = copy.deepcopy(x)
 
 
-result = 1.0
-bin_str = binary(x)
+  result = 1.0
+  bin_str = binary(x)
 
-bin_mantissa = bin_str[9:32]
-res_mantissa = bin_mantissa[:7-exp]    
-result += calc_mantissa(res_mantissa)
+  bin_mantissa = bin_str[9:32]
+  res_mantissa = bin_mantissa[:7-exp]
+  result += calc_mantissa(res_mantissa)
 
-bin_exp = bin_str[1:9]
-exp_int = calc_exp(bin_exp, exp)
-result *= 2**exp_int
+  bin_exp = bin_str[1:9]
+  exp_int = calc_exp(bin_exp, exp)
+  result *= 2**exp_int
 
-if bin_str[0] == '1':
-  result *= -1
+  if bin_str[0] == '1':
+    result *= -1
 
-return result
+  return result
 
 
 def bisection_quantization(num, bits = 7):
@@ -117,12 +117,12 @@ def round_dt8(x, exp = 4):
 def quantize_rowwise(x: torch.Tensor, dt = False):
   abso = torch.abs(x)
   output_maxs  = torch.max(abso,1)[0].unsqueeze(-1)
-  output = x  / output_maxs[0,None]
+  output = x  / output_maxs[None,:]
   if not dt:
       output.apply_(round_fp8)
   else:
       output.apply_(round_dt8)
-  return output, output_maxs
+  return torch.squeeze(output), output_maxs
 
 def dequantize_rowwise(x: torch.Tensor, state_x: torch.Tensor):
   output = x * state_x
@@ -138,40 +138,21 @@ def measure_quantization_error(original_tensor, dequantized_tensor):
   abs_error = torch.abs(original_tensor - dequantized_tensor)
   return torch.mean(abs_error), abs_error
 
+def quantize_stable_embedding(x, batch_size, dt = False):
+  if (x.numel() % batch_size != 0):
+    print("Invalid batch size. Batch size should be a divisor of " + str(x.numel()))
+    return 
 
-sigmas = [1,10,100]
-for sig in sigmas:
-  print('===================')
-  print('current sigma: ', sig)
-  fp16_model = nn.Sequential(
-      nn.Linear(64, 64),
-      nn.Linear(64, 64)
-  )
+  flatarg = torch.argsort(x.flatten())
+  indexing = flatarg.reshape((x.numel()//batch_size,batch_size))
 
-  fp16_model.apply(lambda m: init_weights(m, sig))
+  reshapedx = x.flatten()[indexing]
+  output, maxes = quantize_rowwise(reshapedx,dt)
 
-  print("   unquantized")
-  print(fp16_model.state_dict()['0.weight'][0])
-  #print(bnb.triton.quantize_rowwise.quantize_rowwise)
-  testing, testmax = quantize_rowwise(fp16_model.state_dict()['0.weight'])
+  return output.reshape(x.shape), maxes, indexing
 
-  print("quantized")
-  print(testing[0])
+def dequantize_stable_embedding(input, maxes, indexing, dt=False):
+  outreshape = input.reshape(indexing.shape)
 
-  print("dequantized")
-  print(dequantize_rowwise(testing,testmax)[0])
-
-  testing_dt, dt_max = quantize_rowwise(fp16_model.state_dict()['0.weight'], dt = True)
-
-  print("   unquantized")
-  print(fp16_model.state_dict()['0.weight'][0])
-  print("quantized_dt")
-  print(testing_dt[0])
-  print("dequantized")
-  print(dequantize_rowwise(testing_dt,dt_max)[0])
-
-  fp8_mae, fp8_err = measure_quantization_error(fp16_model.state_dict()['0.weight'], dequantize_rowwise(testing,testmax)[0])
-  print("fp8 mean abs error: ", fp8_mae)
-
-  dt8_mae, dt8_err = measure_quantization_error(fp16_model.state_dict()['0.weight'], dequantize_rowwise(testing_dt,testmax)[0])
-  print("dt8 mean abs error: ", dt8_mae)
+  dequant = dequantize_rowwise(outreshape, maxes)
+  return dequant.reshape(input.shape)
